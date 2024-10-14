8fdd148dc529854a35606964797240138dd30d71
mikensothomas <thomasmikenson20@gmail.com>
2024-10-14 08:25:05 -0300

    Commit

--- sistema_de_venda_de_passagem.py
+++ sistema_de_venda_de_passagem.py
@@ -12,7 +12,7 @@ bloqueio = threading.Lock()
 def produtor():
     while True:
         with bloqueio:
-            for _ in range(20):
+            for _ in range(1000):
                 dados_passagem = {
                     "nome": dados_falsos.name(),
                     "cpf": dados_falsos.cpf(),
@@ -37,7 +37,7 @@ def consumidor():
                     dados = fila.get()
                     fila.task_done()
                     print(f"Demanda consumida: {dados}")
-        time.sleep(3)
+        time.sleep(10)
 
 if __name__ == "__main__":
     thread_produtor = threading.Thread(target=produtor)
